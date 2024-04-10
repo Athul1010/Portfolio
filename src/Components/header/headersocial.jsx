d4fbdc027362ec14bc726a038878c86049bb54b1
Athul1010 <athulrajtsuresh1999@gmail.com>
2024-04-10 14:44:32 +0530

    color changed

--- src/Components/header/headersocial.jsx
+++ src/Components/header/headersocial.jsx
@@ -5,8 +5,8 @@ import { FaGithub } from 'react-icons/fa'
 const Headersocial = () => {
   return (
     <div className='header__socials'>
-    <a href="https://www.linkedin.com/in/athulraj-t/" target="_blank"><BsLinkedin/></a>
-    <a href="https://github.com/Athul1010?tab=repositories" target="_blank"><FaGithub/></a>
+    <a className='icons' href="https://www.linkedin.com/in/athulraj-t/" target="_blank"><BsLinkedin/></a>
+    <a className='icons' href="https://github.com/Athul1010?tab=repositories" target="_blank"><FaGithub/></a>
     </div>
   )
 }
